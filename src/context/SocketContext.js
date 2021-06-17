2d5962d2a0f409112ccad74eabe7ca54dcc11f57
Wendell Smith <wendell18cuenta@gmail.com>
2021-06-17 18:08:17 -0500

    Page Success

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -4,7 +4,9 @@ import { useSocket } from "../hooks/useSocket";
 export const SockectContext = createContext();
 
 export const SockectProvider = ({ children }) => {
-  const { socket, online } = useSocket("http://localhost:8080");
+  const { socket, online } = useSocket(
+    "https://socket-io-bandames.herokuapp.com/"
+  );
 
   return (
     <SockectContext.Provider value={{ socket, online }}>
